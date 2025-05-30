f9ea0a30d7a93517814da425c0338aefb1f915d5
Felipe de Carvalho Santos <142360153+Felipinho5@users.noreply.github.com>
2025-05-30 09:41:49 -0300

    Continuação programação linear matricial

--- Python/Programação linear matricial/main.py
+++ Python/Programação linear matricial/main.py
@@ -2,35 +2,36 @@ from copy import deepcopy
 
 
 class Problema:
-    matriz_restricoes = [
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5],
-        [1, 2, 3, 4, 5],
-    ]
-    resultante = [
-        [63],
-        [55],
-        [4],
-    ]
+    def __init__(self, restricoes, resultante):
+        self.restricoes = restricoes
+        self.adicionar_folgas_nas_restricoes()
+        self.resultante = resultante
+
+    def adicionar_folgas_nas_restricoes(self):
+        for i, linha in enumerate(self.restricoes):
+            for j in range(len(self.restricoes)):
+                folga = 1 if i == j else 0
+                linha.append(folga)
 
     def matriz_3x3(self, vars):
         pass
 
 
 class Matriz:
-    def __init__(self, matriz):
+    def __init__(self, problema, matriz):
         if len(matriz) != 3 or any(len(linha) != 3 for linha in matriz):
             raise ValueError("A matriz deve ser 3x3.")
 
+        self.problema = problema
         self.matriz = matriz
 
     def substituir_coluna_com_resultante(self, num_coluna):
         matriz_subst = deepcopy(self.matriz)
 
         for i in range(3):
-            matriz_subst[i][num_coluna] = Problema.resultante[i][0]
+            matriz_subst[i][num_coluna] = self.problema.resultante[i][0]
 
-        return self.__class__(matriz_subst)
+        return self.__class__(self.problema, matriz_subst)
 
     def sarrus(self):
         matriz_sarrus = deepcopy(self.matriz)
@@ -80,11 +81,21 @@ class Matriz:
 
         return vars
 
+p1 = Problema([
+    [2, 3],
+    [2, 3],
+    [2, 3],
+], [
+    [10],
+    [10],
+    [10],
+])
 
-m1 = Matriz([
+m1 = Matriz(p1, [
     [1, 0, 0],
     [0, 1, 0],
     [0, 0, 1],
 ])
 
 print(m1.cramer())
+print(p1.restricoes)
